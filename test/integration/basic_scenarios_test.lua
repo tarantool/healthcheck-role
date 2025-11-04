7216f984dfbb3c46ca6389e3e9118f1e928a49c4
Satbek Turganbayev <s.turganbayev@corp.mail.ru>
2025-11-04 20:45:52 +0500

    add default checks

--- test/integration/basic_scenarios_test.lua
+++ test/integration/basic_scenarios_test.lua
@@ -3,7 +3,7 @@
 local t = require('luatest')
 local helpers = require('test.helpers.integration')
 local cbuilder = require('luatest.cbuilder')
-local details = require('details')
+local details = require('details_consts')
 local http_client = require('http.client')
 
 ---@type luatest.group
